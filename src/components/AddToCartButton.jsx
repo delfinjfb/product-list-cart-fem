5008bfeff38b88ac0863e706f42cff42ca5f9b2d
Delfin F <DelfinoDevShark@gmail.com>
2024-07-29 19:41:06 +0200

    fav icon and full rounded button

--- src/components/AddToCartButton.jsx
+++ src/components/AddToCartButton.jsx
@@ -1,15 +1,17 @@
 // src/components/AddToCartButton.jsx
 import React from "react";
+import {FaShoppingCart} from "react-icons/fa";
 
-const AddToCartButton = ({ onAddToCart }) => {
-  return (
-    <button
-      onClick={onAddToCart}
-      className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600 mt-2 w-full"
-    >
-      Add to Cart 
-    </button>
-  );
+const AddToCartButton = ({onAddToCart}) => {
+	return (
+		<button
+			onClick={onAddToCart}
+			className="flex items-center justify-center bg-white text-orange-500  px-4 py-2 rounded-full hover:bg-orange-600 border border-orange-500 -m-5"
+		>
+			<FaShoppingCart className="mr-2" />
+			<span>Add to Cart</span>
+		</button>
+	);
 };
 
 export default AddToCartButton;
