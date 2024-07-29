6d89c9d698d257f944cddd17cfcb7b8bece610ff
Delfin F <DelfinoDevShark@gmail.com>
2024-07-29 18:34:39 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+		"./public/fonts/**/*.{ttf}" // Add this line
+	],
+
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
