f70a0fdc1467daa9776bc4518ea87f19e08c3a87
Delfin F <DelfinoDevShark@gmail.com>
2024-07-30 02:48:50 +0200

    AddToCartButton: add or sustract

--- src/components/AddToCartButton.jsx
+++ src/components/AddToCartButton.jsx
@@ -1,16 +1,44 @@
 // src/components/AddToCartButton.jsx
 import React from "react";
-import {FaShoppingCart} from "react-icons/fa";
+import {FaShoppingCart, FaPlus, FaMinus} from "react-icons/fa";
 
-const AddToCartButton = ({onAddToCart}) => {
+const AddToCartButton = ({
+	item,
+	cartItem,
+	onAddToCart,
+	onAddItem,
+	onRemoveItem
+}) => {
 	return (
-		<button
-			onClick={onAddToCart}
-			className="flex items-center justify-center bg-white text-orange-500  px-4 py-2 rounded-full hover:bg-orange-600 border border-orange-500 -m-5"
-		>
-			<FaShoppingCart className="mr-2" />
-			<span>Add to Cart</span>
-		</button>
+		<div>
+			{!cartItem ? (
+				<button
+					onClick={onAddToCart}
+					className="flex items-center justify-center bg-white text-orange-500  px-4 py-2 rounded-full hover:bg-orange-600 border border-orange-500 -m-5"
+				>
+					<FaShoppingCart className="mr-2" />
+					<span>Add to Cart</span>
+				</button>
+			) : (
+				<div className="flex items-center bg-orange-500 rounded-full -m-5 px-4 py-2">
+					<button
+						onClick={onRemoveItem}
+						className="p-2 bg-orange-500 text-white rounded-full hover:bg-white hover:text-orange-500"
+					>
+						<FaMinus />
+					</button>
+					<span className="px-4 bg-orange-500 text-white">
+						{cartItem.quantity}
+					</span>
+					<button
+						onClick={onAddItem}
+						className="p-2 bg-orange-500 text-white rounded-full hover:bg-white hover:text-orange-500"
+					>
+						<FaPlus />
+					</button>
+				</div>
+			)}
+		</div>
 	);
 };
 
--- src/components/DessertItem.jsx
+++ src/components/DessertItem.jsx
@@ -2,24 +2,30 @@
 import React from "react";
 import AddToCartButton from "./AddToCartButton.jsx";
 
-const DessertItem = ({dessert, onAddToCart}) => {
-	return (
-		<div className="  rounded-lg  flex flex-col items-center">
-			<img
-				src={dessert.image}
-				alt={dessert.name}
-				className="rounded-md w-full object-cover h-60"
-			/>
-			<AddToCartButton onAddToCart={() => onAddToCart(dessert)} />
-			<div className="mt-9 w-full ">
-				<h4 className="text-sm">{dessert.category}</h4>
-				<h3 className="text-lg font-medium">{dessert.name}</h3>
-				<p className="text-orange-500 font font-bold ">
-					${dessert.price.toFixed(2)}
-				</p>
-			</div>
-		</div>
-	);
+const DessertItem = ({ dessert, cartItem, onAddToCart, onAddItem, onRemoveItem }) => {
+  return (
+    <div className="rounded-lg flex flex-col items-center relative">
+      <img
+        src={dessert.image}
+        alt={dessert.name}
+        className="rounded-md w-full object-cover h-60"
+      />
+      <AddToCartButton
+        item={dessert}
+        cartItem={cartItem}
+        onAddToCart={() => onAddToCart(dessert)}
+        onAddItem={() => onAddItem(dessert)}
+        onRemoveItem={() => onRemoveItem(dessert)}
+      />
+      <div className="mt-9 w-full ">
+        <h4 className="text-sm">{dessert.category}</h4>
+        <h3 className="text-lg font-medium">{dessert.name}</h3>
+        <p className="text-orange-500 font font-bold ">
+          ${dessert.price.toFixed(2)}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default DessertItem;
