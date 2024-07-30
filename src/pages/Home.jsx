01b2de44218ee6cda7bfbb6b7bbd53b7cc805ac7
Delfin F <DelfinoDevShark@gmail.com>
2024-07-30 03:28:58 +0200

    Meta tags. Modal confirmation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from "react";
 import DessertItem from "../components/DessertItem.jsx";
 import Cart from "../components/Cart.jsx";
+import OrderConfirmationModal from "../components/OrderConfirmationModal.jsx";
+import MetaTags from "../components/MetaTags.jsx"; // Import the MetaTags component
 
 const Home = () => {
 	const [desserts, setDesserts] = useState([]);
 	const [cartItems, setCartItems] = useState([]);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	useEffect(() => {
 		fetch("/data.json")
@@ -44,14 +47,22 @@ const Home = () => {
 	};
 
 	const handleConfirmOrder = () => {
-		alert("Order confirmed!");
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
 		setCartItems([]);
 	};
 
 	return (
 		<div className="container mx-auto py-8 flex flex-col lg:flex-row lg:justify-between">
+			<MetaTags
+				title="Delicious Desserts"
+				description="Enjoy our delicious desserts, including cakes, cookies, pastries, and more."
+			/>
 			<div className="w-full lg:w-2/3">
-				<h1 className="text-3xl font-bold mb-4">Our Delicious Desserts</h1>
+				<h1 className="text-3xl font-bold mb-4">Desserts</h1>
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 					{desserts.map(dessert => (
 						<DessertItem
@@ -72,6 +83,11 @@ const Home = () => {
 					onConfirmOrder={handleConfirmOrder}
 				/>
 			</div>
+			<OrderConfirmationModal
+				isOpen={isModalOpen}
+				onRequestClose={closeModal}
+				cartItems={cartItems}
+			/>
 		</div>
 	);
 };
