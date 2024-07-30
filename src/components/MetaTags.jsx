86ed28776d18278955983ad30a155c8071b89d58
Delfin F <DelfinoDevShark@gmail.com>
2024-07-30 03:28:34 +0200

    MetaTags component creation

---
+++ src/components/MetaTags.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Helmet} from "react-helmet";
+
+const MetaTags = ({title, description}) => {
+	return (
+		<Helmet>
+			<meta charSet="utf-8" />
+			<title>{title}</title>
+			<meta name="description" content={description} />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta
+				name="keywords"
+				content="desserts, bakery, cakes, cookies, pastries"
+			/>
+			<meta name="author" content="Your Bakery Name" />
+			{/* Add more meta tags as needed */}
+		</Helmet>
+	);
+};
+
+export default MetaTags;
